bcbaa6e3b10b79220c6c1259fa6e86f8cab3dd67
David Armendariz <darmendariz1998@outlook.com>
2021-04-03 12:20:56 -0500

    Added accepts_nested_attributes_for technologies

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
-  enum status: { draft: 0, published: 1 }
   extend FriendlyId
+  enum status: { draft: 0, published: 1 }
   friendly_id :title, use: :slugged
   validates_presence_of :title, :body
   belongs_to :topic
