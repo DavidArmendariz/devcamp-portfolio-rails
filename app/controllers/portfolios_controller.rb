0e1e94fefa1e13698ec760ac290f907deda032e9
David Armendariz <darmendariz1998@outlook.com>
2021-04-24 19:29:28 -0500

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: %i[edit show update destroy]
   layout 'portfolio'
+  access all: %i[show index angular], user: { except: %i[destroy new create update edit] }, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.all
@@ -14,13 +16,9 @@ class PortfoliosController < ApplicationController
     3.times { @portfolio_item.technologies.build }
   end
 
-  def edit
-    @portfolio_item = Portfolio.find(params[:id])
-  end
+  def edit; end
 
-  def show
-    @portfolio_item = Portfolio.find(params[:id])
-  end
+  def show; end
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
@@ -34,7 +32,6 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -45,7 +42,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
     respond_to { |format| format.html { redirect_to portfolios_url, notice: 'Record was removed.' } }
   end
@@ -54,5 +50,9 @@ class PortfoliosController < ApplicationController
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
